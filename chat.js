a8b21189a8a990db8408fb75654f4ab77565a7c9
suletha <suletha.gopi@gmail.com>
2023-12-18 21:19:53 +0530

    chat_msg_page_refresh

--- chat.js
+++ chat.js
@@ -3,38 +3,30 @@ const chatMessages = document.querySelector(".chat-messages");
 // const roomName=document.getElementById('room-name');
 const userList = document.getElementById("users");
 
-//get username and room from url
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
+//retrieving token from local storage
+const token = localStorage.getItem("token");
+//user not logged in
+if (!token) {
+  window.location.href = "login.html";
+}
+console.log(token);
+axios.defaults.headers.common["Authorization"] = `${token}`;
 
-const socket = io();
-
-//join chatroom
-socket.emit("joinRoom", { username, room });
-
-//get room and users
-socket.on("roomUsers", ({ room, users }) => {
-  outputRoomName(room);
-  outputUsers(users);
-});
-
-//Message from server
-socket.on("message", (message) => {
-  console.log(message);
-  outputMessage(message);
-
-  //scroll down
-  chatMessages.scrollTop = chatMessages.scrollHeight;
-});
 
 //message submit
-chatForm.addEventListener("submit", (e) => {
+chatForm.addEventListener("submit", async (e) => {
   e.preventDefault();
   const msg = e.target.elements.msg.value;
+  console.log(msg);
 
   //emit message to the server
-  socket.emit("chatMessage", msg);
+  //   socket.emit("chatMessage", msg);
+
+  const response = await axios.post(`http://localhost:3000/msg`, {
+    msg: msg,
+  });
+  console.log(response.data);
+  outputMessage(response.data.msg);
 
   //clear input
   e.target.elements.msg.value = "";
@@ -45,21 +37,49 @@ chatForm.addEventListener("submit", (e) => {
 function outputMessage(message) {
   const div = document.createElement("div");
   div.classList.add("message");
-  div.innerHTML = `<p class="meta">${message.username} <span>${message.time}</span></p>
+  div.innerHTML = `<p class="meta">${message.name} <span>${message.createdAt}</span></p>
     <p class="text">
-        ${message.text}
+        ${message.msg}
     </p>`;
-  document.querySelector(".chat-messages").appendChild(div);
+  chatMessages.appendChild(div);
 }
 
-//Add room name to dom
-function outputRoomName(room) {
-  roomName.innerText = room;
-}
+
 
 //Add users to dom
-function outputUsers(users) {
-  userList.innerHTML = `${users
-    .map((user) => `<li>${user.username}</li>`)
-    .join("")}`;
+function outputUsers(userData) {
+  const user = document.createElement("li");
+  user.innerHTML = `<li>${userData.name}</li>`;
+  userList.appendChild(user);
 }
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const users = await axios.get("http://localhost:3000/users/all");
+    console.log("users", users.data);
+    for (let i = 0; i < users.data.length; i++) {
+      outputUsers(users.data[i]);
+    }
+
+    const msgs = await axios.get("http://localhost:3000/msg/all");
+    console.log("msgs", msgs.data);
+    for (let i = 0; i < msgs.data.length; i++) {
+      outputMessage(msgs.data[i]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+setInterval(async () => {
+  try {
+    chatMessages.innerHTML = "";
+    const msgs = await axios.get("http://localhost:3000/msg/all");
+    console.log("msgs", msgs.data);
+    for (let i = 0; i < msgs.data.length; i++) {
+      outputMessage(msgs.data[i]);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}, 1000);
